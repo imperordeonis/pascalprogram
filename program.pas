3ffd415eaa232ce56ab51a286cfb5257a7cbe9cd
imperordeonis <57955083+imperordeonis@users.noreply.github.com>
2019-11-30 22:20:57 +0200

    Додано end після case of

--- program.pas
+++ program.pas
@@ -8,11 +8,12 @@ read(b);
 write('Виберіть операцію: ');
 read(c);
 
-case c of
+case с of
     1: writeln('Результат: ', a+b);
     2: writeln('Результат: ', a-b);
     3: writeln('Результат: ', a*b);
     4: writeln('Результат: ', a/b);
     else 
     writeln('Шо ти вводиш?');
+end;
 end;
