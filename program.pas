cd633280fe4c6512164a2888c5ca739e42a6e8af
imperordeonis <57955083+imperordeonis@users.noreply.github.com>
2019-11-30 22:35:54 +0200

    NORMALNO CHTO YA SDELAL

--- program.pas
+++ program.pas
@@ -1,18 +1,15 @@
 program Calculator;
-var a,b,c:integer ; 
+var a,b:integer ; 
 begin
 write('Введи число a: ');
 read(a);
 write('Введи число b: ');
 read(b);
-write('Виберіть операцію: ');
-read(c);
 
-case с of
-    1: writeln('Результат: ', a+b);
-    2: writeln('Результат: ', a-b);
-    3: writeln('Результат: ', a*b);
-    4: writeln('Результат: ', a/b);
+case b of
+    1: writeln('Результат: ', a*24*3600);
+    2: writeln('Результат: ', a*24*7*3600);
+    3: writeln('Результат: ', a*24*365*3600);
     else 
     writeln('Шо ти вводиш?');
 end;
